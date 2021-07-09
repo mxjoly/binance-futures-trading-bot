6c18719af440135ecc9de57e2eae4de89e3a26d3
Maxime Joly <maxime.joly2808@gmail.com>
2021-07-09 13:41:27 +0200

    Little change

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import express from 'express';
 import winston from 'winston';
 import Binance from 'binance-api-node';
 import { Bot } from './bot';
@@ -17,18 +16,8 @@ const binanceClient = Binance({
   apiSecret: process.env.BINANCE_PRIVATE_KEY,
 });
 
-const app = express();
-const port = process.env.PORT || 3000;
-
 // ****************************************************************************** //
 
-app.get('**', (req, res) => {
-  const TradingBot = new Bot(binanceClient, tradeConfigs);
-  TradingBot.prepare();
-  TradingBot.run();
-  res.send();
-});
-
-app.listen(port, () => {
-  console.log(`Trading bot is running now at port ${port}`);
-});
+const TradingBot = new Bot(binanceClient, tradeConfigs);
+TradingBot.prepare();
+TradingBot.run();
