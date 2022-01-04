801ab3f9b4e5f93763d334dd993f1f5d74cdf2dc
Maxime Joly <maxime.joly2808@gmail.com>
2022-01-04 17:58:44 +0100

    Remove test mode

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,16 @@ import { tradeConfigs } from './config';
 
 require('dotenv').config();
 
-// To use binance testnet or mainnet
-const TEST_MODE = false;
-
 export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.simple(),
   transports: [new winston.transports.File({ filename: 'logs/bot.log' })],
 });
 
-const binanceClient = TEST_MODE
-  ? Binance({
-      apiKey: process.env.BINANCE_TEST_PUBLIC_KEY,
-      apiSecret: process.env.BINANCE_TEST_PRIVATE_KEY,
-    })
-  : Binance({
-      apiKey: process.env.BINANCE_PUBLIC_KEY,
-      apiSecret: process.env.BINANCE_PRIVATE_KEY,
-    });
+const binanceClient = Binance({
+  apiKey: process.env.BINANCE_PUBLIC_KEY,
+  apiSecret: process.env.BINANCE_PRIVATE_KEY,
+});
 
 // ****************************************************************************** //
 
